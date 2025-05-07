4ed1b2f5b58a2fb5bf922282d529e25ba7cffddf
Michael Kalantar <kalantar@us.ibm.com>
2025-05-07 15:13:16 -0400

    fix controller permissions (#112)
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- internal/controller/modelservice_controller.go
+++ internal/controller/modelservice_controller.go
@@ -136,8 +136,8 @@ func (t *TemplateVars) from(ctx context.Context, msvc *msv1alpha1.ModelService) 
 // +kubebuilder:rbac:groups=inference.networking.x-k8s.io,resources=inferencepool,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups="",resources=services,verbs=list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups="",resources=serviceaccount,verbs=create;update;delete
-// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=rolebinding,verbs=create;update;delete
+// +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
 
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.4/pkg/reconcile
